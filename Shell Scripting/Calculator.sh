67ab6fccac7e8606b1f9410b370bf7816244ed57
Sandra S S <41688158+SandyVR46@users.noreply.github.com>
2022-07-21 22:30:30 +0530

    shell--script--programs

--- Shell Scripting/Calculator.sh
+++ Shell Scripting/Calculator.sh
@@ -1,36 +1,32 @@
-clear
 sum=0
 i="y"
-while [ $i="y" ]
+echo "Enter first number :"
+read n1
+echo "Enter second number :"
+read n2
+while [ $i = "y" ]
 do
-  echo -n "Enter the 1st number: "
-  read n1
-  echo -n "Enter the 2nd number: "
-  read n2
-  echo "******************"
-  echo "1.Addition"
-  echo "2.Substraction"
-  echo "3.Multiplication"
-  echo "4.Division"
-  echo "******************"
-  echo -n "Enter your choice: "
-  read ch
-  case $ch in
-  1)sum=$((n1+n2))
-    echo "Sum = $sum";;
-  2)sum=$((n1-n2))
-    echo "Difference = $sum";;
-  3)sum=$((n1*n2))
-    echo "Product = $sum";;
-  4)sum=$((n1/n2))
-    echo "Answer = $sum";;
-  *)echo "Invalid Input";;
-  esac
-  echo "Do you want to continue[y/n] "
-  echo
-  read i
-  if [ $i != "y" ]
-  then
-    exit
-  fi
+echo "1.Addition"
+echo "2.Subtraction"
+echo "3.Multiplication"
+echo "4.Division"
+echo "Enter your choice"
+read ch
+case $ch in
+1)sum=`expr $n1 + $n2`
+echo "Sum ="$sum;;
+2)sub=`expr $n1 - $n2`
+echo "Sub = "$sub;;
+3)mul=`expr $n1 \* $n2`
+echo "Mul = "$mul;;
+4)div=`echo $n1 / $n2 | bc -l`
+echo "Div = "$div;;
+*)echo "Invalid choice";;
+esac
+echo "Do u want to continue ?"
+read i
+if [ $i != "y" ]
+then
+exit
+fi
 done
